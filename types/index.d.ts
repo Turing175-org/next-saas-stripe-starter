7c18fa67c832bd1133cc729e4b21dcc9e53b3a54
turing0 <turing175@gmail.com>
2024-06-16 13:37:24 +0800

    update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -88,3 +88,40 @@ export type InfoLdg = {
   description: string;
   list: InfoList[];
 }
+
+
+
+
+export interface SearchParams {
+  [key: string]: string | string[] | undefined
+}
+
+export interface Option {
+  label: string
+  value: string
+  icon?: React.ComponentType<{ className?: string }>
+  withCount?: boolean
+}
+
+export interface DataTableFilterField<TData> {
+  label: string
+  value: keyof TData
+  placeholder?: string
+  options?: Option[]
+}
+
+export interface DataTableFilterOption<TData> {
+  id: string
+  label: string
+  value: keyof TData
+  options: Option[]
+  filterValues?: string[]
+  filterOperator?: string
+  isMulti?: boolean
+}
+
+// export type DrizzleWhere<T> =
+//   | SQL<unknown>
+//   | ((aliases: T) => SQL<T> | undefined)
+//   | undefined
+
